4c6c323714afbc2ce72efca27d8605a7a0e1ea50
Anand <anandtrex@users.noreply.github.com>
2017-10-17 01:06:28 +0200

    Moving redis installation to post install hook

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+from setuptools.command.install import install
+from redisinstaller.installer import install_redis, install_redis_json, generate_config, copy_config
 
 FULL_VERSION = "0.2.0"
 
@@ -8,7 +9,19 @@ This file installs the redis.
 
 """
 
+
 ## NOTE: The order of these calls is fairly important. Do not move.
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install_redis()
+        install_redis_json()
+        generate_config()
+        copy_config()
+
+        install.run(self)
 
 
 setup(
@@ -20,14 +33,7 @@ setup(
     description="This module installs redis to the current virtual environment bin",
     install_requires=['jinja2', 'pycurl', 'patool', 'pyunpack', 'gitpython', 'redis', 'rejson'],
     provides=['rediscontroller'],
-    # data_files=[
-    #     ('config', ['config/redis.conf']),
-    # ]
+    cmdclass={
+        'install': PostInstallCommand,
+    },
 )
-
-from redisinstaller.installer import install_redis, install_redis_json, generate_config, copy_config
-
-install_redis()
-install_redis_json()
-generate_config()
-copy_config()
