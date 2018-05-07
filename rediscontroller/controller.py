128dccc38c38f0245906654e02f683369ecea446
maharjun <arjun210493@gmail.com>
2018-05-07 17:53:04 +0200

    Improved start method random start (#4)
    * Improved start method random start
    
    Improved robustness with respect to port collisions
    
    * Reduced number of trials for finding random ports

--- rediscontroller/controller.py
+++ rediscontroller/controller.py
@@ -1,27 +1,75 @@
 import os
 import random
 import subprocess
+from numbers import Integral
 
-from rediscontroller.utils import get_install_prefix, which
+from rediscontroller.utils import get_install_prefix, which, changed_dir
+from rediscontroller.utils import reserve_port
 
 REDIS_PORT = 65535
+
+
+def _careful_start_redis(redis_server_path, data_directory, config_file_path, redis_port):
+    """
+    Tries to start redis at the specified directory, port, with the specified
+    config file. Returns None if there was a port conflict and raises a
+    RuntimeError if the redis server wasn't successfully started.
+
+    If successful, returns True
+    """
+    reserved_port = reserve_port(redis_port)
+    if reserved_port is not None:
+        with changed_dir(data_directory):
+            creation_proc_result = subprocess.run([redis_server_path, config_file_path,
+                                                   '--port', str(redis_port)])
+
+        if creation_proc_result.returncode != 0:
+            raise RuntimeError(
+                "The redis server could not be launched:\n"
+                " in directory: {} \n"
+                " on port number: {}\n"
+                " with config file: {}\n"
+                "\n"
+                "The following is the output of the launch command:\n{}"
+                ""
+                .format(data_directory, redis_port, config_file_path, creation_proc_result.stdout))
+
+        while True:
+            if is_redis_running(redis_port=redis_port):
+                break
+
+        return True
+    else:
+        return None
 
 
 def start_redis(data_directory, config_file_path=None, redis_port=REDIS_PORT):
     """
     Start the redis server with the following options:
-    :param data_directory: The directory where redis should construct its data files
-    :param config_file_path: The redis config for redis to use
-        It is highly recommended to use the redis.conf installed with this package which sets up redis properly.
-        Otherwise, make sure the config you pass in has at least rejson enabled. You should also use the port you set
-        in your clients to connect redis clients.
-    :param redis_port: None or int or the string 'random'. If None, redis is started on port 65535. If 'random', redis is
-        started on a random port between 60000 to 65535
+    :param data_directory: The directory where redis should construct its data
+        files. Note that only one instance of a redis server must be active on a
+        particular directory. This is the responsibility of the script calling this
+        function. If multiple instances are run, the database risks getting
+        corrupted.
+    :param config_file_path: The redis config for redis to use It is highly recommended
+        to use the redis.conf installed with this package which sets up redis properly.
+        Otherwise, make sure the config you pass in has at least rejson enabled. You
+        should also use the port you set in your clients to connect redis clients.
+    :param redis_port: int or the string 'random'. The default port that redis is
+        started on is port 65535. If 'random', redis is started on a random port
+        between 60000 to 65535.
     :return: The port number on which the client was started
     """
+    # I've noticed that if a port collision occurs it isn't really detected by
+    # redis if you use daemonize on. it just fails completely silently, i.e.
+    # returns 0 and doesn't construct the server. Moreover, if there is an already
+    # existing server then that is the one that is assumed constructed. This is
+    # particularly a disaster for randomized ports.
+    #
+    # The only way to be sure the port is free is to try binding a socket to it.
+    # Only if a port is free will the redis instance be started on it. This is done
+    # using a helper function reserve_port in utils.py
     os.makedirs(data_directory, exist_ok=True)
-    current_dir = os.getcwd()
-    os.chdir(data_directory)
 
     redis_server_path = which('redis-server')
     assert redis_server_path is not None, "redis-server not found in PATH. Is redis installed?"
@@ -33,17 +81,20 @@ def start_redis(data_directory, config_file_path=None, redis_port=REDIS_PORT):
     assert os.path.exists(config_file_path), "File {} does not exist".format(config_file_path)
 
     if redis_port == 'random':
-        redis_port = random.randint(60000, 65535)
+        n_random_trials = 4
+        for i in range(n_random_trials):  # makes n_random_trials tries to start a redis server at a random port
+            redis_port = random.randint(60000, 65535)
+            result = _careful_start_redis(redis_server_path, data_directory, config_file_path, redis_port)
+            if result:
+                break
+        else:
+            raise RuntimeError("Tried {} Random ports between 60000-65535, could not find a free port"
+                               .format(n_random_trials))
     else:
-        assert isinstance(redis_port, int), "The redis port should be one of None, the string 'random' or an integer"
-
-    result = subprocess.run([redis_server_path, config_file_path, '--port', str(redis_port)])
-    assert result.returncode == 0
-    os.chdir(current_dir)
-
-    while True:
-        if is_redis_running(redis_port=redis_port):
-            break
+        assert isinstance(redis_port, Integral), "The redis port should be one of the string 'random' or an integer"
+        result = _careful_start_redis(redis_server_path, data_directory, config_file_path, redis_port)
+        if not result:
+            raise RuntimeError("It appears there is a port conflict on the specified port {}".format(redis_port))
 
     return redis_port
 
--- rediscontroller/utils.py
+++ rediscontroller/utils.py
@@ -1,4 +1,18 @@
 import os
+from contextlib import contextmanager, closing
+
+from socket import socket, SOL_SOCKET, SO_REUSEADDR
+from socket import error as SocketError
+
+
+@contextmanager
+def changed_dir(dirname):
+    try:
+        cwd = os.getcwd()
+        os.chdir(dirname)
+        yield
+    finally:
+        os.chdir(cwd)
 
 
 def which(program):
@@ -24,3 +38,68 @@ def get_install_prefix():
         print("Looking for config in user home ~/.local")
         install_prefix = os.path.expanduser('~/.local')
     return install_prefix
+
+# The function below, is taken from
+#
+# https://github.com/Yelp/ephemeral-port-reserve/blob/master/ephemeral_port_reserve.py
+#
+# It is provided under the MIT license
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Yelp
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+def reserve_port(port, ip='localhost'):
+    """Bind to an ephemeral port, force it into the TIME_WAIT state, and unbind it.
+    This means that further ephemeral port alloctions won't pick this "reserved" port,
+    but subprocesses can still bind to it explicitly, given that they use SO_REUSEADDR.
+    By default on linux you have a grace period of 60 seconds to reuse this port.
+    To check your own particular value:
+    $ cat /proc/sys/net/ipv4/tcp_fin_timeout
+    60
+    By default, the port will be reserved for localhost (aka 127.0.0.1).
+    To reserve a port for a different ip, provide the ip as the first argument.
+    Note that IP 0.0.0.0 is interpreted as localhost.
+
+    """
+    port = int(port)
+    with closing(socket()) as s:
+        s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        try:
+            s.bind((ip, port))
+        except SocketError as e:
+            # socket.error: [Errno 98] Address already in use
+            if e.errno == 98:
+                return None
+            else:
+                raise
+
+        # the connect below deadlocks on kernel >= 4.4.0 unless this arg is greater than zero
+        s.listen(1)
+
+        sockname = s.getsockname()
+
+        # these three are necessary just to get the port into a TIME_WAIT state
+        with closing(socket()) as s2:
+            s2.connect(sockname)
+            s.accept()
+            return sockname[1]
